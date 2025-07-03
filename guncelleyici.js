eb647f314d7aabe9bcaf452a454608f999dc54f5
yvzacrr <yavuzberatacar07@gmail.com>
2025-07-03 21:32:17 +0300

    Fiyatları alma denemesi

--- guncelleyici.js
+++ guncelleyici.js
@@ -4,17 +4,15 @@ import axios from 'axios';
 import fs from 'fs';
 
 async function guncelleVeKaydet() {
-    console.log('Fiyatlar csgobackpack.net API\'sinden çekiliyor... Lütfen bekleyin.');
+    console.log('Fiyatlar csgobackpack.net API\'sinden çekiliyor... Bu işlem birkaç saniye sürebilir, lütfen bekleyin.');
     try {
         // Anahtar gerektirmeyen API'ye istek atıyoruz.
         const response = await axios.get('http://csgobackpack.net/api/GetItemsList/v2/');
 
-        // Eğer gelen veri başarılı ise...
         if (response.data && response.data.success) {
             const fiyatListesi = response.data.items_list;
 
             // Gelen tüm fiyatları, düzgün formatlanmış bir şekilde prices.json dosyasına yaz.
-            // 'null, 2' JSON'un okunaklı olması için boşluk bırakmasını sağlar.
             fs.writeFileSync('prices.json', JSON.stringify(fiyatListesi, null, 2));
 
             console.log(`\nBAŞARILI!`);
@@ -22,7 +20,7 @@ async function guncelleVeKaydet() {
             console.log("Artık 'git add .', 'git commit' ve 'git push' komutları ile projeni güncelleyebilirsin.");
 
         } else {
-            console.error('[HATA] API yanıtında bir sorun var. Gelen veri beklenen formatta değil.');
+            console.error('[HATA] API yanıtında bir sorun var.');
         }
     } catch (error) {
         console.error('[HATA] Fiyatlar çekilirken bir hata oluştu:', error.message);
